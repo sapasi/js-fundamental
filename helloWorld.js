6242e0188348d1186e5d20b45eb1138e383744b8
sapasi <pasisameer97@gmail.com>
2025-06-08 12:24:25 +0530

    JS Fundamental Code

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+// To Print Hello World !!
+
+console.log("hello World !!")
+
+//Datatype
+var cust_name = "Sammer"
+var age = 27
+var isMarried = true
+var siblingsName = ["Rahul","Karishma"]
+
+console.log("Values stored in variable name = "+cust_name+" and its type of "+typeof(cust_name) )
+console.log("Values stored in variable age = "+age+" and its type of "+typeof(age) )
+console.log("Values stored in variable isMarried = "+isMarried+" and its type of "+typeof(isMarried) )
+console.log("Values stored in variable siblingName = "+siblingsName+" and its type of "+typeof(siblingsName) )
